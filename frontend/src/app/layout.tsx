e726131079c00affac7e8e12d1d78409b6bbf32a
nullishr <sobhan.yasami@gmail.com>
2025-12-19 13:45:52 +0330

    next and reactjs updated to patched version, also Tanstack query added and configured

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/layout/Navbar";
+import TanstakProvider from "@/providers/TanstakProvider";
 
 export const metadata: Metadata = {
 	title: "PayStruct",
@@ -15,8 +16,10 @@ export default function RootLayout({
 	return (
 		<html lang='en'>
 			<body className={``}>
-				<Navbar />
-				{children}
+				<TanstakProvider>
+					<Navbar />
+					{children}
+				</TanstakProvider>
 			</body>
 		</html>
 	);
